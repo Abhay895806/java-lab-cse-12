1581a0adaae16d20c92888a22605acddec9ae4cc
Abhay895806 <abhay170705@gmail.com>
2026-02-03 16:18:42 +0530

    hello

---
+++ hello.java
@@ -0,0 +1,8 @@
+void main(){
+    System.out.println("hello Buddy");
+    IO.println("hello Buddy");
+    System.out.print("kasa hoo");
+    
+}
+// smallest datatype char,byte,bool,short,int,long,float,double,String
+//keyboard entry
