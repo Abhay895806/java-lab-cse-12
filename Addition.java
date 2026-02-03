5228643d3a5eaf86f0fab4e1da1afd6cbb77f53f
Abhay895806 <abhay170705@gmail.com>
2026-02-03 16:28:06 +0530

    Add two number

---
+++ Addition.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Addition {
+    public static void main(String args[] ){
+        int a,b,c;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter first no");
+        a = scan.nextInt();
+        System.out.println("enter second no");
+        b = scan.nextInt();
+        c=a+b;
+        System.out.println("sum"+c);
+
+
+    }
+
+    
+}
