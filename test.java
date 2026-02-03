cb804003e41c2b049678bd56d7a29acdd0b34b8c
Abhay895806 <abhay170705@gmail.com>
2026-02-03 16:19:19 +0530

    test

---
+++ test.java
@@ -0,0 +1,11 @@
+class test{
+    public static void main(String args[])
+    {
+        System.out.println("my name is aashirwad kumar ");
+        System.out.println(" CSE 12 Java first lab");
+        System.out.println("my favourate subject is maths");
+            System.out.print(" thank for your coperation");
+        
+
+    }
+}
